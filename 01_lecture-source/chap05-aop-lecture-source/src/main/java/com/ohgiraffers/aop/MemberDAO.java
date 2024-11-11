02887da6587026d768e69e52ff1e8bc3508c28cb
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-11 10:29:04 +0900

    [241111]

---
+++ 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/MemberDAO.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.aop;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class MemberDAO {
+
+    /* 아직 DB 접속은 하지 않은 것이기 때문에 임시 DB 생성 */
+    private final Map<Long, MemberDTO> memberMap;
+
+    public MemberDAO() {
+        memberMap = new HashMap<>();
+        memberMap.put(1L, new MemberDTO(1L, "정은미"));
+        memberMap.put(2L, new MemberDTO(2L, "김관훈"));
+    }
+
+    // 회원 전체 조회
+    public Map<Long, MemberDTO> selectAllMember() {
+        return memberMap;
+    }
+
+    // 회원 번호로 회원 조회
+    public  MemberDTO selectMemberByNo(Long no) {
+
+        MemberDTO selectedMember = memberMap.get(no);
+
+        if(selectedMember == null) {
+            throw new RuntimeException("해당하는 " + no + "번째 회원은 없습니다.");
+        }
+
+        return selectedMember;
+    }
+
+
+
+}
