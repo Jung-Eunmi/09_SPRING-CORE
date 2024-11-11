c18c0721dcbb27d127a2c34a09993e6f86fe3e93
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-11 10:39:30 +0900

    [241111]

--- 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/Application.java
+++ 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/Application.java
@@ -10,7 +10,13 @@ public class Application {
         ApplicationContext context
                 = new AnnotationConfigApplicationContext(ContextConfig.class);
 
-        
+        MemberService memberService = context.getBean("memberService", MemberService.class);
+
+        System.out.println("================selectMembers================");
+        System.out.println(memberService.selectMembers());
+
+        System.out.println("================selectMember================");
+        System.out.println(memberService.selectMember(1L));
 
     }
 
