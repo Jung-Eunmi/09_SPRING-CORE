9b91d3723256284de6e15792da51fd7e6149ace9
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-11 11:20:43 +0900

    [241111]

--- 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/ContextConfig.java
+++ 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/aop/ContextConfig.java
@@ -2,8 +2,10 @@ package com.ohgiraffers.aop;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan("com.ohgiraffers.aop")
 public class ContextConfig {
 
